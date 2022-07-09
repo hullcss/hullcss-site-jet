c8252286d0750bacee7af522eab75372d7cf8dfd
CrimsonTome <crimsontome427@protonmail.com>
2022-07-09 08:58:25 +0000

    added feed, needs updating for posts now

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
 const pluginDate = require('eleventy-plugin-date');
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 module.exports = function(eleventyConfig) {
   eleventyConfig.setUseGitIgnore(false);
   eleventyConfig.addWatchTarget('./src/css/tailwind.css');
   eleventyConfig.addPassthroughCopy('./src/img');
   eleventyConfig.addPlugin(pluginDate);
+  eleventyConfig.addPlugin(pluginRss);
 	  return {
     dir: {
       input: 'src',
